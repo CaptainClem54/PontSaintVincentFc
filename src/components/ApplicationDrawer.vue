a349078548bb1483b727a52e21b691c656a46331
Clément <clement.macheret@orange.fr>
2022-05-28 18:20:38 +0200

    Add menu

--- src/components/ApplicationDrawer.vue
+++ src/components/ApplicationDrawer.vue
@@ -1,16 +1,38 @@
 <template>
     <v-container>
-        <v-app-bar app>
-            <div class="d-flex align-center">
-                <v-img alt="PSV Logo" class="shrink mr-2" contain :src="require('../assets/logo_psv.png')"
-                    transition="scale-transition" width="40" />
-                <v-toolbar-title>Pont-Saint-Vincent-FC</v-toolbar-title>
-            </div>
+        <v-app-bar color="blue accent-3" app>
+            <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+            <v-toolbar-title>Pont-Saint-Vincent-FC</v-toolbar-title>
             <v-spacer></v-spacer>
             <v-btn icon @click="toggleDarkTheme()">
                 <v-icon>mdi-weather-night</v-icon>
             </v-btn>
         </v-app-bar>
+
+        <v-navigation-drawer v-model="drawer" app>
+            <v-row class="text-center">
+                <v-col cols="12">
+                    <v-img :src="require('../assets/logo_psv_hight.png')" class="my-3" contain height="100" />
+                </v-col>
+            </v-row>
+
+            <v-divider></v-divider>
+
+            <v-list dense nav>
+                <v-list-item v-for="item in items" :key="item.title" link>
+                    <v-list-item-icon>
+                        <v-icon>{{ item.icon }}</v-icon>
+                    </v-list-item-icon>
+
+                    <v-list-item-content>
+                        <v-list-item-title>{{ item.title }}</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list>
+
+        </v-navigation-drawer>
+
+
     </v-container>
 </template>
 
@@ -22,5 +44,15 @@ export default {
             this.$vuetify.theme.dark = !this.$vuetify.theme.dark;
         },
     },
+    data: () => ({
+        drawer: null,
+        items: [
+            { title: 'Actualité', icon: 'mdi-information-outline' },
+            { title: 'Séniors', icon: 'mdi-account-group' },
+            { title: 'Entrainements', icon: 'mdi-whistle-outline' },
+            { title: 'Contacts', icon: 'mdi-account-box-outline' },
+        ],
+        right: null,
+    }),
 };
 </script>
